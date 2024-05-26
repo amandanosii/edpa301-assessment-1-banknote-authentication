0bcb7fc8220d0e41bea784149d1199b1b9fe3112
amandanosii <161620087+amandanosii@users.noreply.github.com>
2024-05-26 16:07:19 +0200

    moved away from firebase

--- main.py
+++ main.py
@@ -1,21 +1,64 @@
-# [START edpa301-banknote-auth]
+from typing import Dict
+
+from flask import Flask, request, jsonify
+
 import re
 import cv2
-import json
-import skimage as ski
+import face_recognition
 import numpy as np
 
-from firebase_functions import https_fn, options
-from firebase_admin import initialize_app
+from google.oauth2 import service_account
 from google.cloud import vision
 
-initialize_app()
+app = Flask(__name__)
 
-vision_client = vision.ImageAnnotatorClient()
+credentials = service_account.Credentials.from_service_account_file(
+    './edpa301-banknote-auth.json')
+vision_client = vision.ImageAnnotatorClient(credentials=credentials)
 
-@https_fn.on_request(cors=options.CorsOptions(cors_origins="*", cors_methods=["get"]))
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+
+@app.route('/', methods=['GET'])
+def on_request_example():
+    return "Hello world!"
+
+
+def detect_mandela(image_path):
+    image = face_recognition.load_image_file(image_path)
+
+    # Find all face locations and face encodings in the image
+    face_locations = face_recognition.face_locations(image)
+    face_encodings = face_recognition.face_encodings(image, face_locations)
+
+    # Load a reference image to recognize
+    reference_image_path = './mandela.jpg'
+    reference_image = face_recognition.load_image_file(reference_image_path)
+
+    reference_encoding = face_recognition.face_encodings(reference_image)[0]
+
+    for (top, right, bottom,
+         left), face_encoding in zip(face_locations, face_encodings):
+        # Check if the face matches the reference image
+        matches = face_recognition.compare_faces([reference_encoding],
+                                                 face_encoding)
+
+        if matches[0]:
+            return True
+
+        return False
+
+
+def find_serial_number(google_vision_response: Dict):
+    # Join all extracted text into one large string
+    full_text = google_vision_response["responses"][0]["fullTextAnnotation"][
+        "text"]
+
+    # Define the string to search for
+    serial_number_pattern = r'[A-Z] [A-Z] \d{8} [A-Z]'
+
+    # Count occurrences using regex
+    serial_numbers = re.findall(serial_number_pattern, full_text)
+
+    return serial_numbers
 
 
 def detect_text_using_google_vision(image_content):
@@ -29,7 +72,7 @@ def detect_text_using_google_vision(image_content):
     texts = response.text_annotations
     detected_text = str(texts[0].description) if texts else None
 
-    return detected_text
+    return (detected_text, response)
 
 
 def determine_value_from_text_detection(detected_text):
@@ -63,15 +106,15 @@ def crop_at_banknote_edges(image):
         cropped_image: ndarray
     """
     # Read the image file from the FileStorage object
-    # image_stream = image.stream
     file_bytes = np.asarray(bytearray(image), dtype=np.uint8)
-    
+
     image_array = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
     cv2_img = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
 
     # Apply edge detection to find contours
     edges = cv2.Canny(cv2_img, 30, 150)
-    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL,
+                                   cv2.CHAIN_APPROX_SIMPLE)
 
     # Find the largest contour (presumably the banknote)
     largest_contour = max(contours, key=cv2.contourArea)
@@ -83,7 +126,7 @@ def crop_at_banknote_edges(image):
     image_with_boxes = cv2_img.copy()
 
     # Get the bounding rectangles for all contours
-    bounding_rectangles = [cv2.boundingRect(contour) for contour in contours]
+    # bounding_rectangles = [cv2.boundingRect(contour) for contour in contours]
 
     # Compute the minimum and maximum x, y coordinates of all contours
     min_x = min(contour[:, 0, 0].min() for contour in contours)
@@ -98,65 +141,39 @@ def crop_at_banknote_edges(image):
     cv2.rectangle(image_with_boxes, (x, y), (x + w, y + h), (255, 255, 255), 2)
 
     # Crop the image using the bounding box
-    cropped_image = cv2_img[y:y+h, x:x+w]
+    cropped_image = cv2_img[y:y + h, x:x + w]
 
     return cropped_image
 
 
-def enhance_features(image):
-    """
-    Uses skimage to:
-    Increases highlights,
-    Increases brightness,
-    Decreases exposure,
-    Increases contrast
-    """
-    p2, p98 = np.percentile(image, (2, 98))
-    image = ski.exposure.rescale_intensity(image, in_range=(p2, p98))
-    image = ski.exposure.adjust_gamma(image, gamma=2)
-    image = ski.exposure.adjust_sigmoid(image, cutoff=0.5, gain=8)
-
-    return image
-
-
-def create_histogram(image):
-    """
-    Creates a histogram
-    Returns:
-        hist: array
-        bin_edges: array of dtype float
-    """
-    img_float_ndarray = ski.util.img_as_float(image)
-    img_float_ndarray[img_float_ndarray < 0.4] = -1
-    histogram, bin_edges = np.histogram(img_float_ndarray, bins=256, range=(0, 1))
-    
-    return (histogram, bin_edges)
-
-
-@https_fn.on_request(cors=options.CorsOptions(cors_origins="*", cors_methods=["post"]))
-def authenticate_banknote(req: https_fn.Request) -> https_fn.Response:
-    """Authenticate a banknote image sent via HTTP POST."""
+@app.route('/authenticate_banknote', methods=['POST'])
+def authenticate_banknote():
     # Get the banknote image from the request
-    banknote_image = req.files['img']
+    banknote_image = request.files['img']
     img_content = banknote_image.read()
 
-    detected_text = detect_text_using_google_vision(img_content)
+    detected_text, res = detect_text_using_google_vision(img_content)
 
     if detected_text is None:
-        return https_fn.Response("No text detected in the image", status=400)
+        return jsonify({"error": "No text detected in the image"}), 400
 
+    serial_numbers = find_serial_number(res)
     value = determine_value_from_text_detection(detected_text)
-    cropped_img = crop_at_banknote_edges(img_content)
 
-    feature_enhanced_image = enhance_features(cropped_img)
-    hist, bin_edges = create_histogram(feature_enhanced_image)
+    banknote_image.save("banknote.jpg")
+
+    # Check if the banknote has Mandela's face
+    has_mandela = detect_mandela("banknote.jpg")
 
     res_obj = {
         "value": value,
         "detected_text": detected_text,
-        "res": None
+        "serial_numbers": serial_numbers,
+        "has_mandela": has_mandela
     }
 
-    return https_fn.Response(json.dumps(str(res_obj)))
+    return jsonify(res_obj)
 
-# [END edpa301-banknote-auth]
+
+if __name__ == "__main__":
+    app.run(debug=True)
