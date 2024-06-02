70a47e7b7d4d4b1bc191419de8bd47cac87912c3
amandanosii <161620087+amandanosii@users.noreply.github.com>
2024-06-02 13:37:15 +0200

    final program

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from typing import Dict
 
 from flask import Flask, request, jsonify
 
+import time
 import re
 import cv2
+import csv
 import face_recognition
 import numpy as np
 
+from PIL import Image
 from google.oauth2 import service_account
 from google.cloud import vision
 
@@ -46,17 +49,19 @@ def detect_mandela(image_path):
 
         return False
 
+    return False
 
-def find_serial_number(google_vision_response: Dict):
+
+def find_serial_number(detected_text):
     # Join all extracted text into one large string
-    full_text = google_vision_response["responses"][0]["fullTextAnnotation"][
-        "text"]
+    if detected_text is None:
+        return False
 
     # Define the string to search for
     serial_number_pattern = r'[A-Z] [A-Z] \d{8} [A-Z]'
 
     # Count occurrences using regex
-    serial_numbers = re.findall(serial_number_pattern, full_text)
+    serial_numbers = re.findall(serial_number_pattern, detected_text)
 
     return serial_numbers
 
@@ -72,7 +77,7 @@ def detect_text_using_google_vision(image_content):
     texts = response.text_annotations
     detected_text = str(texts[0].description) if texts else None
 
-    return (detected_text, response)
+    return detected_text
 
 
 def determine_value_from_text_detection(detected_text):
@@ -91,9 +96,17 @@ def determine_value_from_text_detection(detected_text):
     }
 
     # Search for each denomination pattern in the detected text
-    for note_value, pattern in patterns.items():
-        if re.search(pattern, detected_text, flags=re.IGNORECASE):
-            return note_value
+    # count the number of occurrences and the most frequent is the value
+    res = {}
+    for value, pattern in patterns.items():
+        res[value] = len(re.findall(pattern, detected_text))
+
+    # Get the denomination with the highest count
+    max_value = max(res.values())
+    note_value = [k for k, v in res.items() if v == max_value]
+
+    if note_value:
+        return note_value[0]
 
     # Return None if no denomination is found
     return None
@@ -152,28 +165,53 @@ def authenticate_banknote():
     banknote_image = request.files['img']
     img_content = banknote_image.read()
 
-    detected_text, res = detect_text_using_google_vision(img_content)
+    detected_text = detect_text_using_google_vision(img_content)
 
     if detected_text is None:
         return jsonify({"error": "No text detected in the image"}), 400
 
-    serial_numbers = find_serial_number(res)
+    serial_numbers = find_serial_number(detected_text)
     value = determine_value_from_text_detection(detected_text)
 
-    banknote_image.save("banknote.jpg")
+    Image.open(banknote_image).save("banknote.jpg")
 
     # Check if the banknote has Mandela's face
     has_mandela = detect_mandela("banknote.jpg")
 
     res_obj = {
-        "value": value,
-        "detected_text": detected_text,
+        "timestamp": time.time(),
         "serial_numbers": serial_numbers,
-        "has_mandela": has_mandela
+        "has_mandela": has_mandela,
+        "value": value,
+        "is_authentic": (has_mandela or serial_numbers) and value is not None
     }
 
-    return jsonify(res_obj)
+    # append results to csv file. create if not existent
+    with open('./static/results.csv', mode='a') as results_file:
+        results_writer = csv.writer(results_file,
+                                    delimiter=',',
+                                    quotechar='"',
+                                    quoting=csv.QUOTE_MINIMAL)
+        results_writer.writerow([
+            res_obj["timestamp"], res_obj["serial_numbers"],
+            res_obj["has_mandela"], res_obj["value"], res_obj["is_authentic"]
+        ])
+
+    return jsonify({
+        "value": res_obj["value"],
+        "is_authentic": res_obj["is_authentic"]
+    })
 
 
 if __name__ == "__main__":
+    with open('./static/results.csv', mode='w') as results_file:
+        results_writer = csv.writer(results_file,
+                                    delimiter=',',
+                                    quotechar='"',
+                                    quoting=csv.QUOTE_MINIMAL)
+        results_writer.writerow([
+            "timestamp", "serial_numbers", "has_mandela", "value",
+            "is_authentic"
+        ])
+
     app.run(debug=True)
